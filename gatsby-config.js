a4258badb90cee44ad47e43bae6b2a3b4a897f44
jaroot32 <jonathanroot15@gmail.com>
2018-09-13 03:20:00 -0700

    first commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,10 +1,10 @@
 module.exports = {
   siteMetadata: {
-    title: "Gatsby Starter - Forty by HTML5 UP",
-    author: "Hunter Chang",
-    description: "A Gatsby.js Starter based on Forty by HTML5 UP"
+    title: "RPD",
+    author: "Fueled",
+    description: "RPDevelopment"
   },
-  pathPrefix: '/',
+  pathPrefix: '/rpdev',
   plugins: [
     {
       resolve: `gatsby-source-filesystem`,
