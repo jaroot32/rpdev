37a4747c207dc1397bf264dc374d77d35ab7cce8
jaroot32 <jonathanroot15@gmail.com>
2018-09-16 03:49:23 -0700

    Our Philosophy Page

--- src/components/BannerLanding.js
+++ src/components/BannerLanding.js
@@ -3,13 +3,13 @@ import React from 'react'
 const BannerLanding = (props) => (
     <section id="banner" className="style2">
         <div className="inner">
-            <header className="major">
+            {/* <header className="major">
                 <h1>Our Philosophy</h1>
             </header>
             <div className="content">
                 <p>Lorem ipsum dolor sit amet nullam consequat<br />
                 sed veroeros. tempus adipiscing nulla.</p>
-            </div>
+            </div> */}
         </div>
     </section>
 )
--- src/pages/ourphilosophy.js
+++ src/pages/ourphilosophy.js
@@ -19,23 +19,26 @@ const AtWork = (props) => (
         <div id="main">
             <section id="one">
                 <div className="inner">
-                    <header className="major">
-                        <h2>Sed amet aliquam</h2>
-                    </header>
-                    <p>Nullam et orci eu lorem consequat tincidunt vivamus et sagittis magna sed nunc rhoncus condimentum sem. In efficitur ligula tate urna. Maecenas massa vel lacinia pellentesque lorem ipsum dolor. Nullam et orci eu lorem consequat tincidunt. Vivamus et sagittis libero. Nullam et orci eu lorem consequat tincidunt vivamus et sagittis magna sed nunc rhoncus condimentum sem. In efficitur ligula tate urna.</p>
+                    {/* <header className="major"> */}
+                        <h1>About Rickabaugh Pentecost Development</h1>
+                    {/* </header> */}
+                    <p>Founded in 2002, Rickabaugh Pentecost Development, LLC (RPD) is a leading Heavy Civil Contractor headquartered in downtown Puyallup.  We serve the Greater Puget Sound region acting as a Site Work General Contractor.</p>
                 </div>
             </section>
             <section id="two" className="spotlights">
                 <section>
-                    <Link to="/generic" className="image">
+                    <Link to="/ourphilosophy" className="image">
                         <img src={ourphilosophy} alt="" />
                     </Link>
+                </section>
+                <section>
                     <div className="content">
                         <div className="inner">
-                            <header className="major">
-                                <h3>Orci maecenas</h3>
-                            </header>
-                            <p>Nullam et orci eu lorem consequat tincidunt vivamus et sagittis magna sed nunc rhoncus condimentum sem. In efficitur ligula tate urna. Maecenas massa sed magna lacinia magna pellentesque lorem ipsum dolor. Nullam et orci eu lorem consequat tincidunt. Vivamus et sagittis tempus.</p>
+                            {/* <header className="major"> */}
+                                <h2>Investing in People</h2>
+                            {/* </header> */}
+                            <p>We are a team of like-minded individuals who thrive on hard work, collaboration, and perfection.  
+                                We are a team that understands what trust means.  Both with those we work with and those we work for.</p>
                             <ul className="actions">
                                 {/* <li><Link to="/generic" className="button">Learn more</Link></li> */}
                             </ul>
@@ -43,31 +46,84 @@ const AtWork = (props) => (
                     </div>
                 </section>
                 <section>
-                    <Link to="/generic" className="image">
+                    <div className="content">
+                        <div className="inner">
+                            <p>This seemingly simple recipe for success is just that, simple.  The recipe is our core purpose and that is to Invest In People.
+                                That purpose is why we exist, why we put 110% effort into every day.  This investment reaches farther than our team.
+                                It reaches our trade partners, our clients, the municipalities we work in, and all those we contact by wanting each individual to be better for having dealt with the company.
+                            </p>
+                            <p>This purpose is derived from a set of non-negotiable core values.  
+                                These values do not change with clients, market conditions, or work load:
+                            </p>
+                            <ul>
+                                <li>Leading by Serving</li>
+                                <li>Building Strong Family and Community</li>
+                                <li>Teaching, Mentoring, and Training</li>
+                                <li>Great attitudes -- Great Teams</li>
+                                <li>Quality Through Excellence</li>
+                            </ul>
+                            <ul className="actions">
+                                {/* <li><Link to="/generic" className="button">Learn more</Link></li> */}
+                            </ul>
+                        </div>
+                    </div>
+                </section>
+                <section>
+                    <Link to="/ourphilosophy" className="image">
                         <img src={ourphilosophy2} alt="" />
                     </Link>
-                    <div className="content">
-                        <div className="inner">
-                            <header className="major">
-                                <h3>Rhoncus magna</h3>
-                            </header>
-                            <p>Nullam et orci eu lorem consequat tincidunt vivamus et sagittis magna sed nunc rhoncus condimentum sem. In efficitur ligula tate urna. Maecenas massa sed magna lacinia magna pellentesque lorem ipsum dolor. Nullam et orci eu lorem consequat tincidunt. Vivamus et sagittis tempus.</p>
-                            <ul className="actions">
-                                {/* <li><Link to="/generic" className="button">Learn more</Link></li> */}
-                            </ul>
-                        </div>
-                    </div>
-                </section>
-                <section>
-                    <Link to="/generic" className="image">
-                        <img src={ourphilosophy3} alt="" />
-                    </Link>
-                    <div className="content">
+                    {/* <div className="content">
                         <div className="inner">
                             <header className="major">
                                 <h3>Sed nunc ligula</h3>
                             </header>
                             <p>Nullam et orci eu lorem consequat tincidunt vivamus et sagittis magna sed nunc rhoncus condimentum sem. In efficitur ligula tate urna. Maecenas massa sed magna lacinia magna pellentesque lorem ipsum dolor. Nullam et orci eu lorem consequat tincidunt. Vivamus et sagittis tempus.</p>
+                            <ul className="actions">
+                                <li><Link to="/generic" className="button">Learn more</Link></li>
+                            </ul>
+                        </div>
+                    </div> */}
+                </section>
+                <section>
+                    <div className="content">
+                        <div className="inner">
+                            {/* <header className="major"> */}
+                                <h2>Commitment to Safety</h2>
+                            {/* </header> */}
+                            <p>
+                                Safety is a conscience, non-negotiable commitment of
+                                RPD.  This commitment is made between the employees, the subcontractors and vendors.
+                                As a company, RPD and its teammembers follow strict guidelines to ensure all disciplines
+                                of work are in accordance with federal, state and local jurisdictions.  RPD instills accountability
+                                within its ranks to achieve its safety goals.
+                            </p>
+                            <p>Because of its emphasis on safety, RPD is proud of the following: 2007, 2008, 2009, 2010, 2011
+                                L&I Experience Factor of .6000 Non-management Safety Steering Team 100% comprehensive drug testing program.
+                            </p>
+                            <ul className="actions">
+                                {/* <li><Link to="/generic" className="button">Learn more</Link></li> */}
+                            </ul>
+                        </div>
+                    </div>
+                </section>
+                <section>
+                    <Link to="/ourphilosophy" className="image">
+                        <img src={ourphilosophy3} alt="" />
+                    </Link>
+                </section>
+                <section>
+                    <div className="content">
+                        <div className="inner">
+                            {/* <header className="major"> */}
+                                <h2>The Paradigm for Quality and Excellence in Construction and Development</h2>
+                            {/* </header> */}
+                            <p>
+                                With great attitudes come great teams with a passion for integrity dedication
+                                to services.  We will lead by serving, sacraficing and mentoring both in-house within our community.
+                                We will be renowned for striving for perfection in every aspect of our daiy operations, while 
+                                having positive impacts on the lives we touch.  The name "RPD" will
+                                be synonymous with quality workmanship, quality citizenship and the leader in the industry.
+                            </p>
                             <ul className="actions">
                                 {/* <li><Link to="/generic" className="button">Learn more</Link></li> */}
                             </ul>
