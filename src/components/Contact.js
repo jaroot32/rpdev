7465389045fca75269cdc59fc1dd4202d139cb6d
jaroot32 <jonathanroot15@gmail.com>
2018-09-21 03:21:43 -0700

    new logos and footer changes

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -26,25 +26,26 @@ const Contact = (props) => (
             <section className="split">
                 <section>
                     <div className="contact-method">
-                        <span className="icon alt fa-envelope"></span>
-                        <h3>Email</h3>
+                        <span className="icon alt"></span>
+                        {/* <h3>Email</h3> */}
                         <a href="mailto:info@rpdevelopment.com">info@rpdevelopment.com</a>
                     </div>
                 </section>
                 <section>
                     <div className="contact-method">
-                        <span className="icon alt fa-phone"></span>
-                        <h3>Phone</h3>
-                        <span>(253) 604-7200</span>
+                        <span className="icon alt"></span>
+                        {/* <h3>Phone</h3> */}
+                         <a href="tel:2536047200"><span>(253) 604-7200</span></a>
                     </div>
                 </section>
                 <section>
-                    <div className="contact-method">
-                        <span className="icon alt fa-home"></span>
-                        <h3>Rickabaugh Pentecost Development, LLC</h3>
+                  <a href="https://goo.gl/maps/79WqCmaWAut">  <div className="contact-method">
+                        <span className="icon alt"></span>
+                        <h4>Rickabaugh Pentecost Development, LLC</h4>
                         <span>108 W Stewart<br />
                         Puyallup, WA 98371<br /></span>
                     </div>
+                  </a>
                 </section>
             </section>
         </div>
